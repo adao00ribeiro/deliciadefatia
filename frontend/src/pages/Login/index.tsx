700b562caa880a9a77c93191ae1c0a2e3b863764
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-06-29 17:13:05 -0300

    att login

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './styles.module.scss'
+import React, { ChangeEvent, FormEvent, useContext, useEffect, useState } from 'react'
+import Link from 'next/link'
+
+
+export default function Login() {
+
+    return (
+        <>
+            <Head>
+                <title>Acessar</title>
+                <meta name="description" content=" app delicia de fatia" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.containerCenter}>
+                <Image src="/FatiaPizza.svg" alt='Logo Fatia de Pizza' width={200} height={200} />
+                <div className={styles.containerLogin}>
+                    <form >
+                        {/*INPUT VAI AKI */}
+
+                        {/*BUTTON VAI AKI */}
+                    </form>
+                    <Link href={'/registrar'}> Não possui uma conta? Cadastre-se</Link>
+                </div>
+            </div>
+        </>
+    )
+}
