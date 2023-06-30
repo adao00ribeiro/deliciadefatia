e5ab7c195790497169f6a0d4f1841cc7a819fcc9
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-06-30 17:41:45 -0300

    add _document

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Login from './Login'
+import Login from './login'
 
 export default function Home() {
   return (
