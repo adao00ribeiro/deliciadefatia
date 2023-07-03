f043b5e4f2d102035edf1e139e30fd65c45420b8
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-07-03 17:25:05 -0300

    att

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,39 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+
 
 @Injectable()
 export class UserService {
-   
-  constructor(private readonly prisma: PrismaService) {}
 
-  create(data: CreateUserDto) {
-    return this.prisma.user.create({
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email }
+    })
+    if (user) {
+      throw new Error("Email Ja existe");
+    }
+
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+    const createdUser = await this.prisma.user.create({
       data
     });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
   findOne(id: string) {
     return this.prisma.user.findUnique({
-      where:{id},
+      where: { id },
     })
   }
 
   update(id: string, data: UpdateUserDto) {
     return this.prisma.user.update({
-      where:{id},
+      where: { id },
       data
     })
   }
 
   remove(id: string) {
     return this.prisma.user.delete({
-      where:{id},
+      where: { id },
     })
   }
 }
