65ade527f1a73bcd80af7a8a18d8134cc058456c
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-07-02 22:50:18 -0300

    Header

---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { useContext } from "react"; 
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { FiLogOut } from 'react-icons/fi'
+
+
+export function Header() {
+     
+    return (
+        < header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Link href="/dashboard">
+                    <img src="/logo.svg" width={190} height={60}></img>
+                </Link>
+                <nav className={styles.menuNav}>
+                    <Link href="/category">
+                     Categoria
+                    </Link>
+                    <Link href="/product">
+                        Cardapio
+                    </Link>
+                    <button >
+                        <FiLogOut color="#FFF" size={24} />
+                    </button>
+                </nav>
+            </div>
+        </header >
+    )
+}
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from './styles.module.scss'
 import React, { ChangeEvent, FormEvent, useContext, useEffect, useState } from 'react'
+import { Header } from '../../Components/Header'
 
 
 
@@ -14,9 +15,21 @@ export default function Dashboard() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className={styles.containerCenter}>
+            <div>
+                <Header />
+                <main>
+                    <div>
+                        <h1>Ultimos pedidos</h1>
+                        <button>
 
+                        </button>
+                    </div>
 
+                    <article>
+
+                    </article>
+
+                </main>
             </div>
         </>
     )
