bd2fda2752eaa0b6f99b6f0bff4cfd878da10a34
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-07-07 17:43:08 -0300

    feat(cadastrarFuncionario): att

--- frontend/src/Components/CadastrarFuncionario/index.tsx
+++ frontend/src/Components/CadastrarFuncionario/index.tsx
@@ -6,6 +6,33 @@ import styles from "./styles.module.scss";
 export function CadastrarFuncionario() {
 
     return (
-        <h1>Cadastrar Funcionario</h1>
+        <div className={styles.containerCadastrar}>
+            <h1>Cadastrar Funcionário</h1>
+            <form className={styles.form}>
+                <div className={styles.group}>
+                    <label htmlFor="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" required /><br /><br />
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="cargo">Cargo:</label>
+                    <select id="cargo" name="cargo" required>
+                        <option value="">Selecione o cargo</option>
+                        <option value="gerente">Gerente</option>
+                        <option value="caixa">Caixa</option>
+                        <option value="pizzaiolo">Pizzaiolo</option>
+                        <option value="garcom">Garçom</option>
+                    </select><br /><br />
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="email" id="email" name="email" required /><br /><br />
+                </div>
+                <div className={styles.group}>
+                    <label htmlFor="telefone">Telefone:</label>
+                    <input type="tel" id="telefone" name="telefone" required /><br /><br />
+                </div>
+                <input type="submit" value="Cadastrar" />
+            </form>
+        </div>
     )
 }
