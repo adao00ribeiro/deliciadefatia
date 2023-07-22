291b2514ad7d60668193bd411f882b48c65db772
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-07-21 22:18:14 -0300

    UPDATE: REGISTRO DE CATEGORIA

--- frontend/src/Components/CadastrarCategoria/index.tsx
+++ frontend/src/Components/CadastrarCategoria/index.tsx
@@ -1,20 +1,36 @@
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
 import styles from "./styles.module.scss";
+import RegisterCategory from "../../services/funcoes/RegisterCategory";
 
-/*
-1 fazer o tratamento do input e do botao
-2 fazer chamada api para registro da categoria
-3 tratar possiveis erros do retorno da api
-*/
+
 export function CadastrarCategoria() {
+    const [InputCategoria,setInputCategoria] = useState('');
 
+    const handleInput = useCallback(function (event : ChangeEvent<HTMLInputElement>) {
+     const {value} = event.target;
+     setInputCategoria(value);
+    },[setInputCategoria])
+
+    const handleSubmit=  useCallback( function (event: FormEvent) {
+        event.preventDefault();
+          RegisterCategory(InputCategoria);
+       },[InputCategoria])
     return (
         <div className={styles.containerCategoria}>
             <h1>Nova Categoria</h1>
-            <form className={styles.form}>
-                <Input type="text" id="nome" name="nome" required placeholder="Digite o nome para a categoria" />
-                <Button >Cadastrar</Button>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <Input 
+                value={InputCategoria} 
+                onChange={handleInput}
+                type="text" 
+                id="nome" 
+                name="nome" 
+                placeholder="Digite o nome para a categoria" 
+                required 
+                />
+                <Button type="submit" >Cadastrar</Button>
             </form>
         </div>
     )
--- frontend/src/services/funcoes/RegisterCategory.ts
+++ frontend/src/services/funcoes/RegisterCategory.ts
@@ -1,8 +1,13 @@
 
+import { toast } from "react-toastify";
 import { api } from "../apiClient";
 
 
 export default async (categoria: string) => {
-
-
+    try {
+        const response = await api.post('/category', {name:categoria});
+        toast.success(response.data.name +' registrado');
+    } catch (e) {
+        toast.error(e.response.data.message);
+    }
 }
